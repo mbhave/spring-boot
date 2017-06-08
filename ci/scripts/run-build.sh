af12596b25404d268457452b36e6bd2bc2fdf153
Madhura Bhave <mbhave@pivotal.io>
2017-06-08 12:06:24 -0700

    Hacking

--- ci/scripts/run-build.sh
+++ ci/scripts/run-build.sh
@@ -1,11 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-cd java-cache-test
-docker build -t springio/java-cache-resource .
+cp -r maven-repo-cache/m2 ~/.m2
+cd git-repo
+mvn install -DskipTests
 
-#cp -r maven-repo-cache/m2 ~/.m2
-#cd git-repo
-#mvn install -DskipTests
-#
-#cp -r ~/.m2 m2
+cp -r ~/.m2/* m2
 
