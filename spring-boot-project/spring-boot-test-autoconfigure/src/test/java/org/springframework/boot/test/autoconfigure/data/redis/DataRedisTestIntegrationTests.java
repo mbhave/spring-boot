e8ec31112884fe817b77573be32157125c08830a
Madhura Bhave <mbhave@pivotal.io>
2017-12-21 10:32:30 -0800

    Hacking on test containers

--- spring-boot-project/spring-boot-test-autoconfigure/src/test/java/org/springframework/boot/test/autoconfigure/data/redis/DataRedisTestIntegrationTests.java
+++ spring-boot-project/spring-boot-test-autoconfigure/src/test/java/org/springframework/boot/test/autoconfigure/data/redis/DataRedisTestIntegrationTests.java
@@ -16,21 +16,30 @@
 
 package org.springframework.boot.test.autoconfigure.data.redis;
 
+import java.io.File;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
+import org.testcontainers.containers.DockerComposeContainer;
 
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.testsupport.rule.RedisTestServer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.ClassUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -43,8 +52,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DataRedisTest
 public class DataRedisTestIntegrationTests {
 
-	@Rule
-	public RedisTestServer redis = new RedisTestServer();
+	@ClassRule
+	public static DockerComposeContainer environment =
+			new DockerComposeContainer(new File("src/test/resources/docker-compose.yml"))
+					.withExposedService("redis", 6379);
+
+//	@Rule
+//	public RedisTestServer redis = new RedisTestServer();
 
 	@Rule
 	public ExpectedException thrown = ExpectedException.none();
@@ -62,6 +76,7 @@ public class DataRedisTestIntegrationTests {
 
 	@Test
 	public void testRepository() {
+		testConnection(createConnectionFactory("localhost"));
 		PersonHash personHash = new PersonHash();
 		personHash.setDescription("Look, new @DataRedisTest!");
 		assertThat(personHash.getId()).isNull();
@@ -76,6 +91,31 @@ public class DataRedisTestIntegrationTests {
 	public void didNotInjectExampleService() {
 		this.thrown.expect(NoSuchBeanDefinitionException.class);
 		this.applicationContext.getBean(ExampleService.class);
+	}
+
+	private RedisConnectionFactory createConnectionFactory(String hostname) {
+		RedisConnectionFactory cf;
+		String hostName = System.getProperty("redis.hostname", hostname);
+		cf = new LettuceConnectionFactoryConfiguration().createConnectionFactory(hostName);
+		testConnection(cf);
+		return cf;
+	}
+
+	private void testConnection(RedisConnectionFactory connectionFactory) {
+		connectionFactory.getConnection().close();
+	}
+
+	private static class LettuceConnectionFactoryConfiguration {
+
+		RedisConnectionFactory createConnectionFactory(String hostName) {
+			LettuceClientConfiguration config = LettuceClientConfiguration.builder()
+					.shutdownTimeout(Duration.ofMillis(0)).build();
+			LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(
+					new RedisStandaloneConfiguration(hostName), config);
+			connectionFactory.afterPropertiesSet();
+			return connectionFactory;
+		}
+
 	}
 
 }
