6899515dbce7f203994f83ba444e16f8aec76195
Madhura Bhave <mbhave@pivotal.io>
2018-04-11 15:52:29 -0700

    Hacking

--- ci/scripts/determine-email-body.sh
+++ ci/scripts/determine-email-body.sh
@@ -15,8 +15,8 @@ if [[ $STATE == "success" ]];then
 	echo "Build SUCCESSFUL ${BUILD_PIPELINE_NAME} / ${BUILD_JOB_NAME} / ${BUILD_NAME}" > email-details/subject
 	if [[ $WAS_PREV_SUCCESSFUL == "" ]];then
     	echo "Build ${ATC_EXTERNAL_URL}/teams/${BUILD_TEAM_NAME}/pipelines/${BUILD_PIPELINE_NAME}/jobs/${BUILD_JOB_NAME}/builds/${BUILD_NAME} is successful!" > email-details/body
-	elif [[ $WAS_PREV_SUCCESSFUL == "success" ]];then
-		echo "" > email-details/body
+#	elif [[ $WAS_PREV_SUCCESSFUL == "success" ]];then
+#		echo "" > email-details/body
 	fi
 elif [[ $STATE == "failure" ]];then
 	echo "Build ${ATC_EXTERNAL_URL}/teams/${BUILD_TEAM_NAME}/pipelines/${BUILD_PIPELINE_NAME}/jobs/${BUILD_JOB_NAME}/builds/${BUILD_NAME} has failed!" > email-details/body
