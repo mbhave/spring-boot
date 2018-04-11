a12b22dc2d3796e72efd886bc612d65a9f559cf2
Madhura Bhave <mbhave@pivotal.io>
2018-04-11 15:31:51 -0700

    hacking

--- ci/scripts/determine-email-body.sh
+++ ci/scripts/determine-email-body.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
-PREV_SHA=$(git rev-parse HEAD^1)
-PREV_STATUSES=$(curl https://api.github.com/repos/spring-projects/spring-boot/commits/$PREV_SHA/statuses)
-PREV_STATES=echo $PREV_STATUSES | jq -r '.[]  | select(.context == "build") | .state'
-WAS_PREV_SUCCESSFUL=$(echo $PREV_STATES | grep 'success')
+pushd git-repo > /dev/null
+	PREV_SHA=$(git rev-parse HEAD^1)
+	echo $PREV_SHA
+popd > /dev/null
+
+#PREV_STATUSES=$(curl https://api.github.com/repos/spring-projects/spring-boot/commits/$PREV_SHA/statuses)
+#PREV_STATES=echo $PREV_STATUSES | jq -r '.[]  | select(.context == "build") | .state'
+#WAS_PREV_SUCCESSFUL=$(echo $PREV_STATES | grep 'success')
+WAS_PREV_SUCCESSFUL=success
 
 if [[ $STATE == "success" ]];then
 	echo "Build SUCCESSFUL ${BUILD_PIPELINE_NAME} / ${BUILD_JOB_NAME} / ${BUILD_NAME}" > email-details/subject
