f9799cc93c3a16ff50de84550c97afc6ffa03cb6
Madhura Bhave <mbhave@pivotal.io>
2018-04-11 17:33:25 -0700

    Hacking

--- ci/scripts/determine-email-body.sh
+++ ci/scripts/determine-email-body.sh
@@ -9,20 +9,20 @@ popd > /dev/null
 #PREV_STATUSES=$(curl https://api.github.com/repos/spring-projects/spring-boot/commits/$PREV_SHA/statuses)
 #PREV_STATES=echo $PREV_STATUSES | jq -r '.[]  | select(.context == "build") | .state'
 #WAS_PREV_SUCCESSFUL=$(echo $PREV_STATES | grep 'success')
-WAS_PREV_SUCCESSFUL=success
+#WAS_PREV_SUCCESSFUL=success
 
 if [[ $STATE == "success" ]];then
-	echo "Build SUCCESSFUL ${BUILD_PIPELINE_NAME} / ${BUILD_JOB_NAME} / ${BUILD_NAME}" > email-details/subject
+	echo "Build SUCCESSFUL ${BUILD_PIPELINE_NAME} / ${BUILD_JOB_NAME} " > email-details/subject
 	if [[ $WAS_PREV_SUCCESSFUL == "" ]];then
-    	echo "Build ${ATC_EXTERNAL_URL}/teams/${BUILD_TEAM_NAME}/pipelines/${BUILD_PIPELINE_NAME}/jobs/${BUILD_JOB_NAME}/builds/${BUILD_NAME} is successful!" > email-details/body
+    	echo "Build https://ci.spring.io/teams/spring-boot/pipelines/${BUILD_PIPELINE_NAME}/jobs/${BUILD_JOB_NAME}/builds/ is successful!" > email-details/body
 	elif [[ $WAS_PREV_SUCCESSFUL == "success" ]];then
 		touch email-details/body
 	fi
 elif [[ $STATE == "failure" ]];then
-	echo "Build ${ATC_EXTERNAL_URL}/teams/${BUILD_TEAM_NAME}/pipelines/${BUILD_PIPELINE_NAME}/jobs/${BUILD_JOB_NAME}/builds/${BUILD_NAME} has failed!" > email-details/body
+	echo "Build https://ci.spring.io/teams/spring-boot/pipelines/${BUILD_PIPELINE_NAME}/jobs/${BUILD_JOB_NAME}/builds/ has failed!" > email-details/body
 	if [[ $WAS_PREV_SUCCESSFUL == "" ]];then
-		echo "Still FAILING ${BUILD_PIPELINE_NAME} / ${BUILD_JOB_NAME} / ${BUILD_NAME}" > email-details/subject
+		echo "Still FAILING ${BUILD_PIPELINE_NAME} / ${BUILD_JOB_NAME} " > email-details/subject
 	elif [[ $WAS_PREV_SUCCESSFUL == "success" ]];then
-		echo "Build FAILURE ${BUILD_PIPELINE_NAME} / ${BUILD_JOB_NAME} / ${BUILD_NAME}" > email-details/subject
+		echo "Build FAILURE ${BUILD_PIPELINE_NAME} / ${BUILD_JOB_NAME} " > email-details/subject
 	fi
 fi
